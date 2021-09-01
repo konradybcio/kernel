86adf15575a6c4cd4113f3931d1542ff3715bd52
Konrad Dybcio <konrad.dybcio@somainline.org>
2021-09-01 22:55:13 +0200

    soc: qcom: socinfo: Add SDM845

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -298,6 +298,10 @@ static struct msm_soc_info cpu_of_id[] = {
 	/* SDM660 ID */
 	[317] = {MSM_CPU_SDM660, "SDM660"},
 
+	/* sdm845 ID */
+	[321] = {MSM_CPU_SDM845, "SDM845"},
+	[341] = {MSM_CPU_SDM845, "SDA845"},
+
 	/* sm8150 ID */
 	[339] = {MSM_CPU_SM8150, "SM8150"},
 
@@ -1211,6 +1215,10 @@ static void * __init setup_dummy_socinfo(void)
 		dummy_socinfo.id = 317;
 		strlcpy(dummy_socinfo.build_id, "sdm660 - ",
 		sizeof(dummy_socinfo.build_id));
+	} else if (early_machine_is_sdm845()) {
+		dummy_socinfo.id = 321;
+		strlcpy(dummy_socinfo.build_id, "sdm845 - ",
+			sizeof(dummy_socinfo.build_id));
 	} else if (early_machine_is_sm8150()) {
 		dummy_socinfo.id = 339;
 		strlcpy(dummy_socinfo.build_id, "sm8150 - ",
