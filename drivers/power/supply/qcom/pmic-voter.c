380689b8b62588b845d348741277d2825d64fcee
Konrad Dybcio <konrad.dybcio@somainline.org>
2022-07-22 17:56:58 +0200

    psy: solve SM5038 PMIC voter mess

--- drivers/power/supply/qcom/pmic-voter.c
+++ drivers/power/supply/qcom/pmic-voter.c
@@ -830,3 +830,56 @@ void destroy_votable(struct votable *votable)
 	kfree(votable->name);
 	kfree(votable);
 }
+
+#if defined(CONFIG_IFPM_SM5038)
+
+ssize_t somc_output_voter_param(struct votable *votable,
+						char *buf, size_t size)
+{
+	int i;
+	int stored_size = 0;
+	char *print_format;
+
+	memset(buf, '\0', size);
+	size--;
+	if (votable->type == VOTE_SET_ANY) {
+		for (i = 0; i < votable->num_clients
+					&& votable->client_strs[i]; i++) {
+			if (!votable->votes[i].enabled)
+				continue;
+
+			if (stored_size == 0)
+				print_format = "%s";
+			else
+				print_format = "; %s";
+			stored_size += scnprintf(buf + stored_size,
+					size - stored_size, print_format,
+					votable->client_strs[i]);
+			if (stored_size >= size)
+				break;
+		}
+	} else {
+		for (i = 0; i < votable->num_clients
+					&& votable->client_strs[i]; i++) {
+			if (!votable->votes[i].enabled)
+				continue;
+
+			if (stored_size == 0)
+				print_format = "%s:%d";
+			else
+				print_format = "; %s:%d";
+			stored_size += scnprintf(buf + stored_size,
+					size - stored_size, print_format,
+					votable->client_strs[i],
+					get_client_vote(votable,
+					votable->client_strs[i]));
+			if (stored_size >= size)
+				break;
+		}
+	}
+	if (stored_size < size)
+		stored_size += scnprintf(buf + stored_size,
+						size - stored_size, "\n");
+	return stored_size;
+}
+#endif
